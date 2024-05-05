3fb2e7f301352918617d9e3edabf4999ba285eea
richer1128 <richer1128@gmail.com>
2024-05-05 22:41:53 +0800

    fix pictureBox.js

--- pictureBox.js
+++ pictureBox.js
@@ -3,7 +3,13 @@ document.addEventListener("DOMContentLoaded", function() {
   
     // 獲取第一張圖片元素
     const firstImage = document.getElementById("firstImage");
+
+      // 獲取LOGO元素
+      const logo = document.querySelector('.logo');
   
-    // 滾動到第一張圖片
-    firstImage.scrollIntoView({ behavior: "smooth" });
+      // 滾動到LOGO元素
+      window.scrollTo({
+          top: logo.offsetTop,
+          behavior: 'smooth'
+      });
   });
