9b7561d24f0dbbedd96e544f9f2d433caf0160cc
richer1128 <richer1128@gmail.com>
2024-05-09 23:55:03 +0800

    css fix

---
+++ Dragging.js
@@ -0,0 +1,16 @@
+var isDragging = false;
+
+document.getElementById("drag-container").addEventListener("pointerdown", function(event) {
+  isDragging = true;
+});
+
+document.addEventListener("pointermove", function(event) {
+  if (isDragging) {
+    event.preventDefault();
+  }
+});
+
+document.addEventListener("pointerup", function(event) {
+  isDragging = false;
+});
+        
